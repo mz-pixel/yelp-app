d4bc67bbc61c46375e4fc7518ec1ec8802824dbc
mz-pixel <63674082+mz-pixel@users.noreply.github.com>
2020-07-17 17:48:10 -0400

    working yelp requests

---
+++ my_yelp.py
@@ -0,0 +1,20 @@
+import requests
+
+api_key = "C06s_2SrxRd2j5Bv3dk5z7xl7uUeiFUo7znDcbQCVjdSeJ99bS-FOKchxUcJSXf1ouwwaRkEQtINN1IsjKEHpbdEp7tmxBrq1P7ABCU1dGKaXRTwtnBoNzVNRAcSX3Yx"
+
+url = "https://api.yelp.com/v3/businesses/search"
+
+my_headers = {
+    "Authorization": f"Bearer {api_key}"    
+}
+
+my_params = {
+    "term": "restaurants",
+    "location": "chicago",
+    "limit": 3,
+}
+
+businesses_object = requests.get(url, params=my_params, headers=my_headers)
+
+businesses_dict = businesses_object.text
+print (businesses_dict)
