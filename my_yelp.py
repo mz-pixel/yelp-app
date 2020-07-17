ff79b32c24d30e6b480e391fd946f6ee5ece85e9
mz-pixel <63674082+mz-pixel@users.noreply.github.com>
2020-07-17 18:09:33 -0400

    Turned my_yelp into a function

--- my_yelp.py
+++ my_yelp.py
@@ -1,20 +1,25 @@
 import requests
 
-api_key = "C06s_2SrxRd2j5Bv3dk5z7xl7uUeiFUo7znDcbQCVjdSeJ99bS-FOKchxUcJSXf1ouwwaRkEQtINN1IsjKEHpbdEp7tmxBrq1P7ABCU1dGKaXRTwtnBoNzVNRAcSX3Yx"
+def search_businesses():
+    
+    api_key = "C06s_2SrxRd2j5Bv3dk5z7xl7uUeiFUo7znDcbQCVjdSeJ99bS-FOKchxUcJSXf1ouwwaRkEQtINN1IsjKEHpbdEp7tmxBrq1P7ABCU1dGKaXRTwtnBoNzVNRAcSX3Yx"
+    
+    url = "https://api.yelp.com/v3/businesses/search"
+    
+    my_headers = {
+        "Authorization": f"Bearer {api_key}"    
+    }
+    
+    my_params = {
+        "term": "restaurants",
+        "location": "chicago",
+        "limit": 3,
+    }
+    
+    businesses_object = requests.get(url, params=my_params, headers=my_headers)
+    
+    businesses_dict = businesses_object.text
+    
+    return businesses_dict
 
-url = "https://api.yelp.com/v3/businesses/search"
-
-my_headers = {
-    "Authorization": f"Bearer {api_key}"    
-}
-
-my_params = {
-    "term": "restaurants",
-    "location": "chicago",
-    "limit": 3,
-}
-
-businesses_object = requests.get(url, params=my_params, headers=my_headers)
-
-businesses_dict = businesses_object.text
-print (businesses_dict)
+invoke_yelp = search_businesses()
