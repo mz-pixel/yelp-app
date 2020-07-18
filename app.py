01c51e7040b94d556ba0c38812fa8e33af98b523
mz-pixel <63674082+mz-pixel@users.noreply.github.com>
2020-07-17 21:04:42 -0400

    Return an index.html page

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return"Hello World!"
+def index():
+    # we need to return a page
+    return render_template("index.html")
     
 my_port = 5000
 
-app.run(host="127.0.0.1", port=my_port, debug=True)
+app.run(host="127.0.0.1", port=my_port, debug=False)
+
